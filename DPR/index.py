11d96965f7034a7d7fb11c0ee80b45806a7f49ae
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-12-06 17:55:05 +0700

    feat: update dpr

--- DPR/index.py
+++ DPR/index.py
@@ -1,252 +1,172 @@
-import pickle
-import os
+import re
+import logging
+import torch
+import faiss
+from tqdm import tqdm
+from typing import List
+from elasticsearch import Elasticsearch
+from transformers import (DPRContextEncoder, DPRContextEncoderTokenizer,
+                          DPRQuestionEncoder, DPRQuestionEncoderTokenizer)
+from chunker import DocumentChunker
+from document import DPRDocument
 
+class DPRIndex(DocumentChunker):
 
-class InvertedIndex:
-    """
-    Class yang mengimplementasikan bagaimana caranya scan atau membaca secara
-    efisien Inverted Index yang disimpan di sebuah file; dan juga menyediakan
-    mekanisme untuk menulis Inverted Index ke file (storage) saat melakukan indexing.
+    '''
+    Class for indexing and searching documents, using a combination of
+    vectors producted by DPR and keyword matching from Elastic TF-IDF. As a
+    subclass of DocumentChunker, this class automatically handles document
+    chunking as well.
+    '''
 
-    Attributes
-    ----------
-    postings_dict: Dictionary mapping:
+    INDEX_NAME = 'dense-passage-retrieval'
+    D = 768
+    context_tokenizer = DPRContextEncoderTokenizer.from_pretrained(
+        'facebook/dpr-ctx_encoder-single-nq-base')
+    context_model = DPRContextEncoder.from_pretrained(
+        'facebook/dpr-ctx_encoder-single-nq-base', return_dict=True)
+    question_tokenizer = DPRQuestionEncoderTokenizer.from_pretrained(
+        'facebook/dpr-question_encoder-single-nq-base')
+    question_model = DPRQuestionEncoder.from_pretrained(
+        'facebook/dpr-question_encoder-single-nq-base', return_dict=True)
 
-            termID -> (start_position_in_index_file,
-                       number_of_postings_in_list,
-                       length_in_bytes_of_postings_list,
-                       length_in_bytes_of_tf_list)
+    def __init__(self, documents: List[DPRDocument]):
+        super(DocumentChunker).__init__()
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if self.device == 'cuda':
+            self.reader_model = self.reader_model.cuda()
+        self.faiss_index = faiss.IndexFlatIP(self.D)
+        self._setup_elastic_index()
+        self._build_index(documents)
 
-        postings_dict adalah konsep "Dictionary" yang merupakan bagian dari
-        Inverted Index. postings_dict ini diasumsikan dapat dimuat semuanya
-        di memori.
+    def _setup_elastic_index(self):
+        '''Sets up the Elastic Index. Deletes old ones if needed.'''
+        self.es = Elasticsearch()
+        if self.es.indices.exists(self.INDEX_NAME):
+            logging.warning(f'Deleting old index for {self.INDEX_NAME}.')
+            self.es.indices.delete(self.INDEX_NAME)
+        self.es.indices.create(index=self.INDEX_NAME)
 
-        Seperti namanya, "Dictionary" diimplementasikan sebagai python's Dictionary
-        yang memetakan term ID (integer) ke 4-tuple:
-           1. start_position_in_index_file : (dalam satuan bytes) posisi dimana
-              postings yang bersesuaian berada di file (storage). Kita bisa
-              menggunakan operasi "seek" untuk mencapainya.
-           2. number_of_postings_in_list : berapa banyak docID yang ada pada
-              postings (Document Frequency)
-           3. length_in_bytes_of_postings_list : panjang postings list dalam
-              satuan byte.
-           4. length_in_bytes_of_tf_list : panjang list of term frequencies dari
-              postings list terkait dalam satuan byte
+    def _build_index(self, documents):
+        '''
+        Initializes the data structure to keep track of which chunks
+        correspond to which documents.
+        '''
+        self.documents = documents
+        self.doc_bodies = [doc.body for doc in self.documents]
+        self.chunks = []
+        self.chunk_index = {}  # {chunk: document}
+        self.inverse_chunk_index = {}  # {document: [chunks]}
+        chunk_counter = 0
+        for doc_counter, doc_body in tqdm(enumerate(self.doc_bodies),
+                                          total=len(self.doc_bodies)):
+            self.inverse_chunk_index[doc_counter] = []
+            chunked_docs = self.chunk_document(doc_body)
+            self.chunks.extend(chunked_docs)
+            for chunked_doc in chunked_docs:
+                chunk_embedding = self.embed_context(chunked_doc)
+                self.faiss_index.add(chunk_embedding)
+                self.es.create(self.INDEX_NAME, id=chunk_counter,
+                               body={'chunk': chunked_doc})
+                self.chunk_index[chunk_counter] = doc_counter
+                self.inverse_chunk_index[doc_counter].append(chunk_counter)
+                chunk_counter += 1
+        self.total_docs = len(self.documents)
+        self.total_chunks = len(self.chunks)
 
-    terms: List[int]
-        List of terms IDs, untuk mengingat urutan terms yang dimasukan ke
-        dalam Inverted Index.
+    def embed_question(self, question: str):
+        '''Embed the question in vector space with the question encoder.'''
+        input_ids = self.question_tokenizer(
+            question, return_tensors='pt')['input_ids']
+        embeddings = self.question_model(
+            input_ids).pooler_output.detach().numpy()
+        return embeddings
 
-    """
+    def embed_context(self, context: str):
+        '''Embed the context (doc) in vector space with the question encoder.'''
+        input_ids = self.context_tokenizer(
+            context, return_tensors='pt')['input_ids']
+        embeddings = self.context_model(
+            input_ids).pooler_output.detach().numpy()
+        return embeddings
 
-    def __init__(self, index_name, postings_encoding, directory=''):
-        """
-        Parameters
-        ----------
-        index_name (str): Nama yang digunakan untuk menyimpan files yang berisi index
-        postings_encoding : Lihat di compression.py, kandidatnya adalah StandardPostings,
-                        GapBasedPostings, dsb.
-        directory (str): directory dimana file index berada
-        """
+    def search_dense_index(self, question: str, k: int = 5):
+        '''
+        Search the vector index by encoding the question and then performing
+        nearest neighbor on the FAISS index of context vectors.
 
-        self.index_file_path = os.path.join(directory, index_name+'.index')
-        self.metadata_file_path = os.path.join(directory, index_name+'.dict')
+        Args:
+            question (str):
+                The natural language question, e.g. `who is bill gates?`
+            k (int):
+                The number of documents to return from the index.
+        '''
+        if k > self.total_chunks:
+            k = self.total_chunks
+        question_embedding = self.embed_question(question)
+        dists, chunk_ids = self.faiss_index.search(question_embedding, k=k)
+        dists, chunk_ids = list(dists[0]), list(chunk_ids[0])
+        dists = list(map(float, dists))  # For Flask
+        structured_response = []
+        for dist, chunk_id in zip(dists, chunk_ids):
+            chunk = self.chunks[chunk_id]
+            document_id = self.chunk_index[chunk_id]
+            document = self.documents[document_id]
+            blob = {
+                'document': document,
+                'document_id': document_id,
+                'chunk': chunk,
+                'chunk_id': int(chunk_id),  # For Flask
+                'faiss_dist': dist
+            }
+            structured_response.append(blob)
+        return structured_response
 
-        self.postings_encoding = postings_encoding
-        self.directory = directory
+    def search_sparse_index(self, query):
+        body = {
+            'size': 10,
+            'query': {
+                'match': {
+                    'chunk': query
+                }
+            }
+        }
+        results = self.es.search(index=self.INDEX_NAME, body=body)
+        hits = results['hits']['hits']
+        return hits
 
-        self.postings_dict = {}
-        self.terms = []         # Untuk keep track urutan term yang dimasukkan ke index
-        # key: doc ID (int), value: document length (number of tokens)
-        self.doc_length = {}
-        # Ini nantinya akan berguna untuk normalisasi Score terhadap panjang
-        # dokumen saat menghitung score dengan TF-IDF atau BM25
-        self.total_doc_length = 0
-
-    def __enter__(self):
-        """
-        Memuat semua metadata ketika memasuki context.
-        Metadata:
-            1. Dictionary ---> postings_dict
-            2. iterator untuk List yang berisi urutan term yang masuk ke
-                index saat konstruksi. ---> term_iter
-            3. doc_length, sebuah python's dictionary yang berisi key = doc id, dan
-                value berupa banyaknya token dalam dokumen tersebut (panjang dokumen).
-                Berguna untuk normalisasi panjang saat menggunakan TF-IDF atau BM25
-                scoring regime; berguna untuk untuk mengetahui nilai N saat hitung IDF,
-                dimana N adalah banyaknya dokumen di koleksi
-
-        Metadata disimpan ke file dengan bantuan library "pickle"
-
-        Perlu memahani juga special method __enter__(..) pada Python dan juga
-        konsep Context Manager di Python. Silakan pelajari link berikut:
-
-        https://docs.python.org/3/reference/datamodel.html#object.__enter__
-        """
-        # Membuka index file
-        self.index_file = open(self.index_file_path, 'rb+')
-
-        # Kita muat postings dict dan terms iterator dari file metadata
-        with open(self.metadata_file_path, 'rb') as f:
-            self.postings_dict, self.terms, self.doc_length, self.total_doc_length = pickle.load(f)
-            self.term_iter = self.terms.__iter__()
-
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        """Menutup index_file dan menyimpan postings_dict dan terms ketika keluar context"""
-        # Menutup index file
-        self.index_file.close()
-
-        # Menyimpan metadata (postings dict dan terms) ke file metadata dengan bantuan pickle
-        with open(self.metadata_file_path, 'wb') as f:
-            pickle.dump([self.postings_dict, self.terms, self.doc_length, self.total_doc_length], f)
-
-
-class InvertedIndexReader(InvertedIndex):
-    """
-    Class yang mengimplementasikan bagaimana caranya scan atau membaca secara
-    efisien Inverted Index yang disimpan di sebuah file.
-    """
-
-    def __iter__(self):
-        return self
-
-    def reset(self):
-        """
-        Kembalikan file pointer ke awal, dan kembalikan pointer iterator
-        term ke awal
-        """
-        self.index_file.seek(0)
-        self.term_iter = self.terms.__iter__()  # reset term iterator
-
-    def __next__(self):
-        """
-        Class InvertedIndexReader juga bersifat iterable (mempunyai iterator).
-        Silakan pelajari:
-        https://stackoverflow.com/questions/19151/how-to-build-a-basic-iterator
-
-        Ketika instance dari kelas InvertedIndexReader ini digunakan
-        sebagai iterator pada sebuah loop scheme, special method __next__(...)
-        bertugas untuk mengembalikan pasangan (term, postings_list, tf_list) berikutnya
-        pada inverted index.
-
-        PERHATIAN! method ini harus mengembalikan sebagian kecil data dari
-        file index yang besar. Mengapa hanya sebagian kecil? karena agar muat
-        diproses di memori. JANGAN MEMUAT SEMUA INDEX DI MEMORI!
-        """
-        curr_term = next(self.term_iter)
-        (postings_list, tf_list) = self.get_postings_list(curr_term)
-        return (curr_term, postings_list, tf_list)
-
-    def get_postings_list(self, term):
-        """
-        Kembalikan sebuah postings list (list of docIDs) beserta list
-        of term frequencies terkait untuk sebuah term (disimpan dalam
-        bentuk tuple (postings_list, tf_list)).
-
-        PERHATIAN! method tidak boleh iterasi di keseluruhan index
-        dari awal hingga akhir. Method ini harus langsung loncat ke posisi
-        byte tertentu pada file (index file) dimana postings list (dan juga
-        list of TF) dari term disimpan.
-        """
-        (start_pos, _, len_in_bytes_of_postings, len_in_bytes_of_tf) = self.postings_dict[term]
-        self.index_file.seek(start_pos)
-        postings_list = self.postings_encoding.decode(
-            self.index_file.read(len_in_bytes_of_postings))
-        tf_list = self.postings_encoding.decode_tf(
-            self.index_file.read(len_in_bytes_of_tf))
-        return (postings_list, tf_list)
-
-
-class InvertedIndexWriter(InvertedIndex):
-    """
-    Class yang mengimplementasikan bagaimana caranya menulis secara
-    efisien Inverted Index yang disimpan di sebuah file.
-    """
-
-    def __enter__(self):
-        self.index_file = open(self.index_file_path, 'wb+')
-        return self
-
-    def append(self, term, postings_list, tf_list):
-        """
-        Menambahkan (append) sebuah term, postings_list, dan juga TF list 
-        yang terasosiasi ke posisi akhir index file.
-
-        Method ini melakukan 4 hal:
-        1. Encode postings_list menggunakan self.postings_encoding (method encode),
-        2. Encode tf_list menggunakan self.postings_encoding (method encode_tf),
-        3. Menyimpan metadata dalam bentuk self.terms, self.postings_dict, dan self.doc_length.
-           Ingat kembali bahwa self.postings_dict memetakan sebuah termID ke
-           sebuah 4-tuple: - start_position_in_index_file
-                           - number_of_postings_in_list
-                           - length_in_bytes_of_postings_list
-                           - length_in_bytes_of_tf_list
-        4. Menambahkan (append) bystream dari postings_list yang sudah di-encode dan
-           tf_list yang sudah di-encode ke posisi akhir index file di harddisk.
-
-        Jangan lupa update self.terms dan self.doc_length juga ya!
-
-        SEARCH ON YOUR FAVORITE SEARCH ENGINE:
-        - Anda mungkin mau membaca tentang Python I/O
-          https://docs.python.org/3/tutorial/inputoutput.html
-          Di link ini juga bisa kita pelajari bagaimana menambahkan informasi
-          ke bagian akhir file.
-        - Beberapa method dari object file yang mungkin berguna seperti seek(...)
-          dan tell()
-
-        Parameters
-        ----------
-        term:
-            term atau termID yang merupakan unique identifier dari sebuah term
-        postings_list: List[Int]
-            List of docIDs dimana term muncul
-        tf_list: List[Int]
-            List of term frequencies
-        """
-        encode_postings = self.postings_encoding.encode(postings_list)
-        encode_tf = self.postings_encoding.encode_tf(tf_list)
-
-        start_pos = self.index_file.tell()
-        self.terms.append(term)
-        self.postings_dict[term] = (start_pos, len(postings_list), len(encode_postings), len(encode_tf))
-
-        for i in range(len(postings_list)):
-            if postings_list[i] in self.doc_length:
-                self.doc_length[postings_list[i]] += tf_list[i]
+    def _merge_results(self, sparse_results, dense_results):
+        '''Merges the results of sparse and dense retrieval.'''
+        results_index = {}
+        for sparse_result in sparse_results:
+            id, score = sparse_result['_id'], sparse_result['_score']
+            id = int(id)
+            results_index[id] = {'elastic_score': score}
+        for dense_result in dense_results:
+            id, score = dense_result['chunk_id'], dense_result['faiss_dist']
+            if id in results_index:
+                results_index[id]['faiss_dist'] = score
             else:
-                self.doc_length[postings_list[i]] = tf_list[i]
-            self.total_doc_length += tf_list[i]
-        
-        self.index_file.write(encode_postings)
-        self.index_file.write(encode_tf)
+                results_index[id] = {'faiss_dist': score}
+        results = []
+        for chunk_id, scores in results_index.items():
+            document_id = self.chunk_index[chunk_id]
+            document = self.documents[document_id]
+            chunk = self.chunks[chunk_id]
+            doc_profile = document.to_dict()
+            result = {
+                'chunk_id': chunk_id,
+                'chunk': chunk,
+                'document_id': document_id,
+                'document': doc_profile,
+                'scores': scores
+            }
+            results.append(result)
+        return results
 
-
-if __name__ == "__main__":
-
-    from compression import VBEPostings
-
-    with InvertedIndexWriter('test', postings_encoding=VBEPostings, directory='./tmp/') as index:
-        index.append(1, [2, 3, 4, 8, 10], [2, 4, 2, 3, 30])
-        index.append(2, [3, 4, 5], [34, 23, 56])
-        index.index_file.seek(0)
-        assert index.terms == [1, 2], "terms salah"
-
-        assert index.doc_length == {
-            2: 2, 3: 38, 4: 25, 5: 56, 8: 3, 10: 30}, "doc_length salah"
-        assert index.postings_dict == {1: (0,
-                                           5,
-                                           len(VBEPostings.encode(
-                                               [2, 3, 4, 8, 10])),
-                                           len(VBEPostings.encode_tf([2, 4, 2, 3, 30]))),
-                                       2: (len(VBEPostings.encode([2, 3, 4, 8, 10])) + len(VBEPostings.encode_tf([2, 4, 2, 3, 30])),
-                                           3,
-                                           len(VBEPostings.encode([3, 4, 5])),
-                                           len(VBEPostings.encode_tf([34, 23, 56])))}, "postings dictionary salah"
-
-        index.index_file.seek(index.postings_dict[2][0])
-        assert VBEPostings.decode(index.index_file.read(
-            len(VBEPostings.encode([3, 4, 5])))) == [3, 4, 5], "terdapat kesalahan"
-        assert VBEPostings.decode_tf(index.index_file.read(
-            len(VBEPostings.encode_tf([34, 23, 56])))) == [34, 23, 56], "terdapat kesalahan"
+    def search_dual_index(self, query: str):
+        '''Search both the sparse and dense indices and merge the results.'''
+        sparse_result = self.search_sparse_index(query)
+        dense_result = self.search_dense_index(query)
+        merged_results = self._merge_results(sparse_result, dense_result)
+        return merged_results
--- README.md
+++ README.md
@@ -6,3 +6,23 @@
 2.  Activate env, for Windows use `env/Scripts/activate.bat`, for MacOS use `source env/bin/activate`
 3.  Install Dependencies
     ```pip install -r requirements.txt```
+
+### Dense Passage Retrieval
+1.  Install ElasticSearch
+    https://www.elastic.co/guide/en/elasticsearch/reference/current/targz.html
+    ```
+    curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.11.1-darwin-x86_64.tar.gz
+    curl https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.11.1-darwin-x86_64.tar.gz.sha512 | shasum -a 512 -c - 
+    tar -xzf elasticsearch-8.11.1-darwin-x86_64.tar.gz
+    cd elasticsearch-8.11.1/ 
+    ```
+2.  Run ElasticSearch Host
+    ```
+    ./bin/elasticsearch
+    ```
+
+### Demo
+Collab Link: https://colab.research.google.com/drive/1FU38Lp7v36dlEgIuzq4ihpoc5BoromlR?usp=sharing
+
+### Presentation Video
+YouTube Link: 
